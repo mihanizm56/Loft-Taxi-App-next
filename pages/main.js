e2ffbecd98cc93938bd6f19f4f48829ffd8ca395
mihanizm56 <m727507.56@mail.ru>
2019-10-06 03:50:12 +0300

    docs(routes): 123
    123
    
    МЕТА ДАННЫЕ: 123

--- pages/main.js
+++ pages/main.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { MainPageApp } from "../src/streams";
+import { handleAuthSSR } from "./utils/authSSR";
 import { withTranslation } from "../i18n";
 
 class Main extends React.Component {
 	static async getInitialProps() {
+		await handleAuthSSR(ctx);
+
 		return {
 			namespacesRequired: ["common"],
 		};
--- pages/utils/authSSR.js
+++ pages/utils/authSSR.js
@@ -1,23 +1,38 @@
 import { fetchAccessTokenRequest } from "../../src/services/api/requests";
 
 export const handleAuthSSR = async ({ ctx }) => {
-	if (ctx.isServer && ctx.req && Boolean(ctx.req.headers.cookie)) {
+	if (ctx.isServer && ctx.res && ctx.req && Boolean(ctx.req.headers.cookie)) {
 		/* eslint-disable */
 		const token = ctx.req.headers.cookie.replace(
 			/(?:(?:^|.*;\s*)access_token\s*\=\s*([^;]*).*$)|^.*$/,
 			"$1"
 		);
 		/* eslint-enable */
+		console.log("pathname", ctx.pathname);
+
+		const isLoginPage = Boolean(ctx.pathname === "/login"); // TODO in constants
 
 		if (token) {
 			try {
 				const { message, error } = await fetchAccessTokenRequest({ token });
 				console.log("resultOfTokenLogin", message, error);
 
-				if (message && !error) {
+				if (message && !error && isLoginPage) {
 					console.log("TOKEN VERIFIED FROM SERVER");
+					// if(ctx.res) {}
 					ctx.res.writeHead(302, {
 						Location: "/main",
+					});
+
+					ctx.res.end();
+					return;
+				}
+
+				if (error) {
+					console.log("TOKEN NOT VALID FROM SERVER");
+					// if(ctx.res) {}
+					ctx.res.writeHead(401, {
+						Location: "/login",
 					});
 
 					ctx.res.end();
@@ -25,35 +40,42 @@ export const handleAuthSSR = async ({ ctx }) => {
 			} catch (error) {
 				console.log("error in prefetch auth with token", error);
 			}
+		} else if (!isLoginPage) {
+			console.log("NO TOKEN IN COOKIES");
+			ctx.res.writeHead(401, {
+				Location: "/login",
+			});
+
+			ctx.res.end();
 		}
 	}
 
 	// если надо на клиенте ловить токен в куке - смотри сюда!!!!!!!!!!!!!!!!!!!!!!!!
 	// else {
-	// 	// get token on the client
+	// // get token on the client
 
-	// 	token = cookies.get("access_token");
-	// 	console.log("test client token", cookies.get("access_token"));
+	// token = cookies.get("access_token");
+	// console.log("test client token", cookies.get("access_token"));
 	// }
 
 	// try {
-	// 	const response = await axios.get(serverUrl + "/api/token/ping", {
-	// 		headers: { Authorization: token },
-	// 	});
-	// 	// dont really care about response, as long as it not an error
-	// 	console.log("token ping:", response.data.msg);
+	// const response = await axios.get(serverUrl + "/api/token/ping", {
+	// headers: { Authorization: token },
+	// });
+	// // dont really care about response, as long as it not an error
+	// console.log("token ping:", response.data.msg);
 	// } catch (err) {
-	// 	// in case of error
-	// 	console.log(err.response.data.msg);
-	// 	console.log("redirecting back to main page");
-	// 	// redirect to login
-	// 	if (ctx.res) {
-	// 		ctx.res.writeHead(302, {
-	// 			Location: "/",
-	// 		});
-	// 		ctx.res.end();
-	// 	} else {
-	// 		Router.push("/");
-	// 	}
+	// // in case of error
+	// console.log(err.response.data.msg);
+	// console.log("redirecting back to main page");
+	// // redirect to login
+	// if (ctx.res) {
+	// ctx.res.writeHead(302, {
+	// Location: "/",
+	// });
+	// ctx.res.end();
+	// } else {
+	// Router.push("/");
+	// }
 	// }
 };
