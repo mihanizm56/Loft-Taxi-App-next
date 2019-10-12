fb3eb450e870e4112f50fc7041153d77f8b5a736
Mihail_Kozhevnikov <makozhevnikov@vtb.ru>
2019-10-12 23:53:21 +0400

    feat(auh): added auth in credentials page
    added auth in the credentials page

--- pages/credentials.js
+++ pages/credentials.js
@@ -1,21 +1,32 @@
 import React from "react";
 import { CredentialsPageApp } from "../src/streams";
-import { i18n, withTranslation } from "../i18n";
+import { handleAuthSSR } from "../src/utils/helpers";
+import { withTranslation } from "../i18n";
 
 class Credentials extends React.Component {
-	static async getInitialProps() {
+	static getInitialProps = async ctx => {
+		console.log("AM ON THE CREDENTIALS PAGE");
+
+		await handleAuthSSR(ctx);
+
 		return {
 			namespacesRequired: ["common"],
 		};
-	}
+	};
 
 	render() {
-		const { isLoading, t } = this.props;
+		const { isLoading } = this.props;
 
-		return (
-			// <div>
-			<CredentialsPageApp isLoading={isLoading} />
-			/* <button
+		return <CredentialsPageApp isLoading={isLoading} />;
+	}
+}
+
+// TODO make splitted dicts
+export default withTranslation("common")(Credentials);
+
+// const { isLoading, t } = this.props;
+// <div>
+/* <button
 					type="button"
 					onClick={() =>
 						i18n.changeLanguage(i18n.language === "en" ? "ru" : "en")
@@ -24,10 +35,4 @@ class Credentials extends React.Component {
 					Change locale
 				</button>
 				<h1>{t("title")}</h1> */
-			// </div>
-		);
-	}
-}
-
-// TODO make splitted dicts
-export default withTranslation("common")(Credentials);
+// </div>
--- pages/login.js
+++ pages/login.js
@@ -10,7 +10,9 @@ class Login extends React.Component {
 
 		await handleAuthSSR(ctx);
 
-		return {};
+		return {
+			namespacesRequired: ["common"],
+		};
 	};
 
 	render() {
